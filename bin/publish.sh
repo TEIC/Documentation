412a970af0518aae4f609f2215568c72c7038cef
raffazizzi <raffaeleviglianti@gmail.com>
2023-11-21 14:38:36 -0500

    updated paths

--- bin/publish.sh
+++ bin/publish.sh
@@ -2,5 +2,5 @@
 
 for f in main/TCW/*.xml; do 
   xmllint --noout $f
-  xsltproc --param CETEI "'/js/CETEI.js'" --param CSS "'/css/tei.css'" make-CETEIcean.xsl $f > "TCW/$(basename -- "$f" .xml).html"; 
+  xsltproc --param CETEI "'/js/CETEI.js'" --param CSS "'/css/tei.css'" main/make-CETEIcean.xsl $f > "TCW/$(basename -- "$f" .xml).html"; 
 done
