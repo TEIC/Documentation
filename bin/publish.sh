e0bd2039930e83b84616535151929806b4c40bfb
raffazizzi <raffaeleviglianti@gmail.com>
2023-11-21 14:20:32 -0500

    cleaning up

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+mkdir -p __output/TCW
+
 for f in TCW/*.xml; do 
   xmllint --noout $f
-  xsltproc --param CETEI "'/js/CETEI.js'" --param CSS "'/css/tei.css'" make-CETEIcean.xsl $f > "teic.github.io/TCW/$(basename -- "$f" .xml).html"; 
+  xsltproc --param CETEI "'/js/CETEI.js'" --param CSS "'/css/tei.css'" make-CETEIcean.xsl $f > "__output/TCW/$(basename -- "$f" .xml).html"; 
 done
